49c72ae406a595841655a62bc0fe4bc90ea9ac99
NOID <q821424508@163.com>
2018-09-28 15:15:49 +0800

    添加测试模块2

---
+++ dubbo-demo/dubbo-demo-my-test/src/main/java/com/alibaba/App.java
@@ -0,0 +1,17 @@
+package com.alibaba;
+
+import java.util.ServiceLoader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App<T>
+{
+    public static void main( String[] args )
+    {
+        java.sql.DriverManager driverManager;
+        ServiceLoader serviceLoader;
+        System.out.println( "Hello World!" );
+    }
+}
